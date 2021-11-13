a0b4b5c24c6a817dc1cb5346f19301e8c2fd73ec
DESKTOP-AQ46HRP\Par <vaslysalex@hotmail.gr>
2021-11-13 23:13:19 +0200

    aws s3 backet

--- app/Http/Controllers/UploadfileController.php
+++ app/Http/Controllers/UploadfileController.php
@@ -27,6 +27,7 @@ class UploadfileController extends BaseController
 
     public function SaveVideo(Request $request)
     {
+        ini_set('max_execution_time', 600);
         request()->validate([
             'videofile' => 'required|file|mimes:mp4|unique:upload_videos,upload_name'
         ]);
@@ -51,9 +52,9 @@ class UploadfileController extends BaseController
             if($ch)
             {return redirect('ManageVideos')->withErrors("Upload failed cause file exists");}
 
-            $path = $request->file('videofile')->storeAs('public/videos', $fileNameToStore);
+            $path = $request->file('videofile')->storeAs('public/videos', $fileNameToStore,'s3');
             DB::table('upload_videos')->insert(['upload_name'=>$fileNameToStore]);
-           
+
 
         }
         else{
@@ -68,10 +69,11 @@ class UploadfileController extends BaseController
     public function DownVideo(Request $request)
     {
         $selected = $request->input('videofileD');
-        if(is_file((storage_path('app/public/videos/' . $selected)))) {
-            return response()->download(storage_path('app/public/videos/' . $selected));
-        }
-        else return redirect('ManageVideos')->withErrors("download failed");
+    //    if(is_file((storage_path('app/public/videos/' . $selected)))) {
+    //        return response()->download(storage_path('app/public/videos/' . $selected));
+          return Storage::disk('s3')->download('public/videos/' . $selected);
+    //    }
+    //     else return redirect('ManageVideos')->withErrors("download failed");
     }
 
 
@@ -79,12 +81,13 @@ class UploadfileController extends BaseController
     {
         $selected = $request->Vid;
         $check = DB::table('upload_videos')->where('upload_name','=',$selected)->exists();
-        $check2=is_file((storage_path('app/public/videos/' . $selected)));
+       // $check2=is_file((storage_path('app/public/videos/' . $selected)));
+       $check2=Storage::disk('s3')->exists('public/videos/'.$selected);
         if($check&&$check2) {
             DB::table('upload_videos')->where('upload_name','=',$selected)->delete();
-            Storage::delete('public/videos/'.$selected);
-           // Schema::drop(substr($selected,0,-4));
-            return ("success");
+           // Storage::delete('public/videos/'.$selected);
+           Storage::disk('s3')->delete('public/videos/' . $selected);
+        return redirect('ManageVideos')->with('success', 'deleted successfully');
         }
         else return redirect('ManageVideos')->withErrors("not found, check again");
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,19 +13,24 @@ class ComposerAutoloaderInitc26a0b17747a5188005af528827f6fb2
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc26a0b17747a5188005af528827f6fb2', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc26a0b17747a5188005af528827f6fb2', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitc26a0b17747a5188005af528827f6fb2::getInitializer($loader));
         } else {
