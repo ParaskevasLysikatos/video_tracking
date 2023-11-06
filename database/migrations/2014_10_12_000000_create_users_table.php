5e2169231e698785e0e0d1d5a575003be2bd5e5e
Paraskevas <vaslysalex@hotmail.gr>
2023-11-06 16:29:43 +0200

    forgot migrations

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('username');
+            $table->string('role');
+            $table->integer('user_session')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
